d47afcdb332525a7c7a18ae24aff092b12a901f2
Ahmed Rashed <ahmedrashed669@gmail.com>
2024-02-03 18:06:04 +0300

    cases list done

--- patients/views.py
+++ patients/views.py
@@ -4,9 +4,11 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render,get_object_or_404,redirect
 from django.views.generic import (ListView,CreateView,UpdateView,DeleteView,DetailView)
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from .forms import PatientCaseForm
+from accounts.models import HospitalEmployee,Hospital
 from .models import *
 
 # Create your views here.
@@ -42,26 +44,34 @@ class PatientDetail(LoginRequiredMixin,DetailView):
     #     query = PatientCase.objects.get(patient_name_id = self.kwargs['pk'])
     #     return query
     #working
-    # def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-    #     context = super().get_context_data(**kwargs)
-    #     query = PatientCase.objects.get(patient_name_id = self.kwargs['pk'])
-    #     context['patientcase'] = query
-    #     return context
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        query = PatientCase.objects.filter(patient_name_id = self.kwargs['pk'])
+        context['patientcase'] = query
+        return context
 
     #Patient Cases
 
 @login_required
 def PatientCaseCreate(request,pk):
     patient = get_object_or_404(Patient,pk=pk)
+    hospital = request.user.hospitalemployee.hospital
+
     if request.method == 'POST':
         form = PatientCaseForm(request.POST)
         if form.is_valid:
             patientcase = form.save(commit=False)
             patientcase.patient_name = patient
-            patientcase.save()
+            patientcase.reported_by = hospital
+            patientcase.save()  
             return redirect('patients:patient-detail',pk=patient.pk)
         
     else:
         form = PatientCaseForm()
 
     return render(request,'patients/patientcase_form.html',{'form':form})        
+
+
+class PatientCaseDetail(LoginRequiredMixin,DetailView):
+    model = PatientCase
+    context_object_name = "patientcase"

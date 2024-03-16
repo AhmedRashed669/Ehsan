4b35a66a07f58a322c671fe4517b50fa804c2c64
Ahmed Rashed <ahmedrashed669@gmail.com>
2024-03-16 14:45:57 +0300

    donors cant sign in to the web application criteria

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,26 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from .models import HospitalEmployee
+from django.contrib import messages
+from django.shortcuts import redirect
 
 # Create your views here.
+
+
+class CustomLoginView(LoginView):
+
+    template_name = "accounts/login.html"
+
+    def form_valid(self, form):
+        """Security check complete. Log the user in."""
+        user = form.get_user()
+
+        # Check if the user is a HospitalEmployee or SystemEmployee
+        if hasattr(user, 'hospitalemployee') or hasattr(user, 'systememployee') or user.is_superuser:
+            return super().form_valid(form)
+        else:
+            messages.error(self.request, 'You are not allowed to login.')
+            return redirect('accounts:login')

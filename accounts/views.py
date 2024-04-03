9d76be5efeebc6da000bb8c52b70822cb9200bda
Ahmed Rashed <ahmedrashed669@gmail.com>
2024-04-03 07:46:24 +0300

    FCM token is being stored on signing in

--- accounts/views.py
+++ accounts/views.py
@@ -7,10 +7,6 @@ from .models import HospitalEmployee
 from django.views.decorators.csrf import csrf_exempt
 import json
 
-
-
-
-
 # Create your views here.
 
 
@@ -31,28 +27,7 @@ class CustomLoginView(LoginView):
             response_data = {'message': False}
             print("Response data: ", response_data)  # Print response data
             return JsonResponse(response_data)
-        # Override this method to customize behavior after successful form validation
 
-    # def form_invalid(self, form):
-    #     #Override this method to customize behavior when form validation fails
-    #     print("Received data: ", form.cleaned_data)  # Print received data
-    #     response_data = {'message': False}
-    #     print("Response data: ", response_data)  # Print response data
-    #     return JsonResponse(response_data)
-
-
-
-
-    # def form_valid(self, form):
-    #     """Security check complete. Log the user in."""
-    #     user = form.get_user()
-
-    #     # Check if the user is a HospitalEmployee or SystemEmployee
-    #     if hasattr(user, 'hospitalemployee') or hasattr(user, 'systememployee') or user.is_superuser:
-    #         return super().form_valid(form)
-    #     else:
-    #         messages.error(self.request, 'You are not allowed to login.')
-    #         return redirect('accounts:login')
 # @csrf_exempt
 # def test(request):
 #     if request.method == 'POST':
@@ -62,3 +37,5 @@ class CustomLoginView(LoginView):
 #             return JsonResponse({'shit': value})
 
 #     return JsonResponse({'eror':'sth wrong'},status = 400)
+
+
--- router.py
+++ router.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
 from patients.api.views import PatientCaseViewSet
 from donors.api.views import DonorViewSet,DonationsViewSet
-
+from fcm_django.api.rest_framework import FCMDeviceCreateOnlyViewSet
 
 router = routers.DefaultRouter()
 router.register('patientcases', PatientCaseViewSet, basename="patientcase")
 router.register('donors', DonorViewSet, basename="donor")
 router.register('donations', DonationsViewSet, basename="donation")
+router.register('devices', FCMDeviceCreateOnlyViewSet)

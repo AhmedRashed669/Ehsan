49fe91f7dc92fbbf12509a4334c249f4fbc76d8d
Ahmed Rashed <ahmedrashed669@gmail.com>
2024-04-12 21:34:19 +0300

    remainder cost is added

--- patients/api/serializer.py
+++ patients/api/serializer.py
@@ -1,13 +1,22 @@
 from rest_framework import serializers
-from patients import models
+from patients import models as patientmodel
+from donors import models as donormodels
+from django.db.models import Sum
 
 class PatientCaseSerializer(serializers.ModelSerializer):
     patient_name = serializers.StringRelatedField()
     # patient_name = serializers.CharField(source = 'patient_name.first_name')
     reported_by = serializers.CharField(source = 'reported_by.hospital_name')
     age = serializers.IntegerField(source = 'patient_name.age')
+    remainder_cost = serializers.SerializerMethodField()
     
     class Meta:
-        model = models.PatientCase
-        fields = ["pk","patient_name","diagnose","cost","case_type","created_date","reported_by","age",]
+        model = patientmodel.PatientCase
+        fields = ["pk","patient_name","diagnose","cost","case_type","created_date","reported_by","age","remainder_cost"]
+
+    def get_remainder_cost(self, obj):
+        total_donation = donormodels.PatientCase_Donors.objects.filter(patient_case=obj).aggregate(total=Sum('amount'))['total']
+        return obj.cost - total_donation
+
+        
 
